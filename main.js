46c717dccd066db9604d576930993cae7069820a
RodriAllende <90284758+RodriAllende@users.noreply.github.com>
2023-10-17 17:35:34 -0300

    vamos

--- main.js
+++ main.js
@@ -1,13 +1,9 @@
-
-
-$(function() {
-  // Obtenemos la fecha y la hora actuales.
-  var fecha = new Date();
-
-  // Formateamos la fecha y la hora en el formato especificado por la configuración regional del navegador.
-  var fechaFormateada = fecha.toLocaleString();
-  var horaFormateada = fecha.toLocaleString("time");
-
-  // Mostramos la fecha y la hora en el DOM.
-  $('#fecha-hora').html(fechaFormateada + " - " + horaFormateada);
+$(document).ready(function() {
+  $(window).on("scroll", function() {
+    if ($(this).scrollTop() > 100) { // Cambia "100" a la altura deseada para el colapso
+      $(".navbar").addClass("navbar-scroll");
+    } else {
+      $(".navbar").removeClass("navbar-scroll");
+    }
+  });
 });
